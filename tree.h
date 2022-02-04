f68da430e94a4485e4ca8a33580e8976a95d0c87
yushoyamaguchi <ysh.824@outlook.jp>
2022-02-04 11:35:16 +0900

    tree

---
+++ tree.h
@@ -0,0 +1,7 @@
+typedef struct  {
+        struct node      *parent;
+        struct node      *child_left;
+        struct node      *child_left;
+        struct in_addr  addr;
+        u_int_32_t      addr_binary;
+}node;
